62a421241e0cef2dc8551524995349aea037f45c
Larshjelen <larshjelen@gmail.com>
2021-04-20 13:55:54 +0200

    Added CurrencyOverview Activity

---
+++ app/src/main/java/no/kristiania/coinhub/CurrencyOverview.kt
@@ -0,0 +1,11 @@
+package no.kristiania.coinhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrencyOverview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency_overview)
+    }
+}
