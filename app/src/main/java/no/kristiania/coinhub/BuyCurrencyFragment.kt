ac822d99e5c973df60cf95892faa3a55d310ecef
amkh <ammarkhalil08@gmail.com>
2021-04-24 15:43:09 +0200

    Textwatcher on editText

--- app/src/main/java/no/kristiania/coinhub/BuyCurrencyFragment.kt
+++ app/src/main/java/no/kristiania/coinhub/BuyCurrencyFragment.kt
@@ -2,6 +2,7 @@ package no.kristiania.coinhub
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.squareup.picasso.Picasso
 import no.kristiania.coinhub.databinding.BuyCurrencyLayoutBinding
@@ -27,5 +28,18 @@ class BuyCurrencyFragment : Fragment(R.layout.buy_currency_layout) {
         binding.currencySymbol.text = symbol
         binding.currencyRateBuy.text = priceUsd.toString()
         Picasso.get().load("https://static.coincap.io/assets/icons/${symbol?.toLowerCase()}@2x.png").into(binding.currencyImageBuy)
+
+        binding.usdInput.addTextChangedListener(){
+            var input = it.toString()
+
+            if(input.isNullOrEmpty()){
+                binding.CurrencyOutput.text = "${symbol}"
+            } else {
+
+                var output = input.toDouble() / priceUsd!!
+                binding.CurrencyOutput.text = output.toString()
+            }
+
+        }
     }
 }
