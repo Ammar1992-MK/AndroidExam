f5f8d404d6303f54d9e2ad65344090f40dc92735
Larshjelen <larshjelen@gmail.com>
2021-05-04 13:11:24 +0200

    eeee

--- app/src/main/java/no/kristiania/coinhub/db/DataBase.kt
+++ app/src/main/java/no/kristiania/coinhub/db/DataBase.kt
@@ -36,7 +36,7 @@ abstract class DataBase : RoomDatabase(){
         val MIGRATION_1_2 = object : Migration(1, 2){
             override fun migrate(database: SupportSQLiteDatabase) {
 
-                database.execSQL("CREATE TABLE `Transaction_History_Table` (`id` INTEGER, `type` TEXT,`volume` INTEGER, `rate` INTEGER, `symbol` TEXT, " +
+                database.execSQL("CREATE TABLE `transaction_history_table` (`id` INTEGER, `type` TEXT,`volume` INTEGER, `rate` INTEGER, `symbol` TEXT, " +
                         "PRIMARY KEY(`id`))")
             }
 
